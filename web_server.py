11a5632017ab40f30bee70a15aaa07091b6e9dd8
Andy Barber <git@commerce.abarber.net>
2022-07-14 16:52:48 -0400

    Use all host addresses

--- web_server.py
+++ web_server.py
@@ -48,5 +48,5 @@ def update_airports():
 
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=int(sys.argv[1]), debug=False)
+    app.run(host="0.0.0.0", port=int(sys.argv[1]), debug=False)
     
